89b90b8f88491484aabee26f98868a14e7cdd979
628hh50 <628hh50@canonslade.tbft.uk>
2026-09-14 11:49:51 +0000

    const sum things

--- Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
@@ -4,9 +4,17 @@ document.getElementById('calculateBtn').addEventListener('click', calculate);
 function calculate() {
     // TODO:
     // 1. Get the three numbers (A, B, C) from the input fields
+    let numA = parseFloat(document.getElementById('numberA').value);
+    let numB = parseFloat(document.getElementById('numberB').value);
+    let numC = parseFloat(document.getElementById('numberC').value);
     // 2. Calculate:
     //    - (A + B + C) / 3
+    const sum = (numA + numB + numC)/3
+
     //    - A × B × C
+    const sum1 = (numA * numB * numC)
     //    - A + (B × C)
+    const sum2 = numA + (numB * numC)
     // 3. Display the results in the respective span elements
+    document.getElementById("average").textContent = sum;
 }
