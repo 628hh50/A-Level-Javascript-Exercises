44b5ff45bb8f0b917a96428d62200dff59b0c43b
628hh50 <628hh50@canonslade.tbft.uk>
2026-09-16 09:45:21 +0000

    finish task 2 1a

--- Class Exercises/2 - Javascript Selection/Task 1a - Golfing Handicap/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1a - Golfing Handicap/script.js
@@ -10,12 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate which league the player belongs to
 function calculateLeague() {
     // TODO: Get the handicap value from the input field
-    document.getElementById('handicap')
+    let handicap = document.getElementById('handicap').value
     // TODO: Check if the handicap is less than 13
+    let league;
     if (handicap < 13) {
-    let league = "1"
-    else 
-        let league = "2"
+    league = "1" }
+    else { 
+    league = "2"
     }
     // TODO: Display the appropriate message in the result paragraph
-}    document.getElementById('result')
+    document.getElementById('result').textContent = `Your league is ${league}`
+}
