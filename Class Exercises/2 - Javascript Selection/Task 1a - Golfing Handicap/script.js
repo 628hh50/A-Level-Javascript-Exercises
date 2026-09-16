d4fe8093e9d6b90006a2d06a2f5b8336eb67e037
628hh50 <628hh50@canonslade.tbft.uk>
2026-09-16 09:26:07 +0000

    2, 1a task, getting handicap value and putting if statement

--- Class Exercises/2 - Javascript Selection/Task 1a - Golfing Handicap/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1a - Golfing Handicap/script.js
@@ -10,8 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate which league the player belongs to
 function calculateLeague() {
     // TODO: Get the handicap value from the input field
-    
+    document.getElementById('handicap')
     // TODO: Check if the handicap is less than 13
+    if (handicap < 13) {
     
+    }
     // TODO: Display the appropriate message in the result paragraph
-}
+}    
