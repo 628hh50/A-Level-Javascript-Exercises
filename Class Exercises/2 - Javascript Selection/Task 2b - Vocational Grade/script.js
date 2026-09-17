7edfa973ec2786ff31cc3523b28d4d7c5ffd0d0a
628hh50 <628hh50@canonslade.tbft.uk>
2026-09-17 14:28:18 +0000

    half way though 2 2b

--- Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
@@ -10,11 +10,20 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the Hang Gliding Certificate grade
 function calculateGrade() {
     // TODO: Get the theory and practical exam scores
-    
+    let practical = parseInt(document.getElementById('practical').value)
+    let theory = parseInt(document.getElementById('theory').value) 
     // TODO: Check if both scores are above 50% (Pass requirement)
-    
+    percentage = (practical + theory)/2
+    if (practical > 50) and (theory >50){
+        grade = "pass"
+    }
     // TODO: If passed, calculate average and check if above 70% (Distinction requirement)
-    
-    // TODO: Display the appropriate grade (Pass, Distinction, or Fail)
+    else if (grade = "pass") and (theory > 70) and (practical > 70)
+        grade = "distinction"
+    }
+    else{ (grade = fail)
+    }   
+        // TODO: Display the appropriate grade (Pass, Distinction, or Fail)
     // Also display the average score
+    document.getElementById('result').textContent = `Your grade is ${grade} and your average score is ${averageScore}`
 }
