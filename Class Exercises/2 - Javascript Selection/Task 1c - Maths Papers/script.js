b95caa1c06c93d276c5b31e795670bfbe48ef7d6
628hh50 <628hh50@canonslade.tbft.uk>
2026-09-17 08:29:04 +0000

    finish task 2 1c

--- Class Exercises/2 - Javascript Selection/Task 1c - Maths Papers/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1c - Maths Papers/script.js
@@ -10,14 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate if student passed or failed
 function calculateResult() {
     // TODO: Get the marks for all three papers
-    document.getElementById('paper1').value
-    document.getElementById('paper2').value
-    document.getElementById('paper3').value
+
+    let paper1 = parseInt(document.getElementById('paper1').value)
+    let paper2 = parseInt(document.getElementById('paper2').value)
+    let paper3 = parseInt(document.getElementById('paper3').value)
+
     // TODO: Calculate the total mark
-    totalMarks = paper1 + paper2 + paper3
+
+    let totalMarks = paper1 + paper2 + paper3
+
     // TODO: Check if total is greater than 160
-    let total;
-    if (total > 160)
-        grade = pass
-    // TODO: Display the total mark and whether the student passed or failed
+    if (totalMarks > 160) {
+        grade = "pass"
+    }
+    else {
+        grade = "fail"
+    }
+        // TODO: Display the total mark and whether the student passed or failed
+        document.getElementById('result').textContent = `You ${grade}ed`
 }
